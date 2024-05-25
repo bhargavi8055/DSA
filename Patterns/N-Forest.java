29b5c3472b40905c9a72b386abd36b0086e1b8de
bhargavi8055 <59086546+bhargavi8055@users.noreply.github.com>
2024-05-25 06:52:31 +0530

    Create  N-Forest.java

---
+++ Patterns/N-Forest.java
@@ -0,0 +1,57 @@
+:::::::::::::::Link for the Problem Statement for practise:::::::::::::::
+
+https://www.naukri.com/code360/problems/n-forest_6570177?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems
+
+:::::::::::::::Problem Statement:::::::::::::::
+Sam is making a forest visualizer. An N-dimensional forest is represented by the pattern of size NxN filled with ‘*’.
+
+For every value of ‘N’, help sam to print the corresponding N-dimensional forest.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+* * *
+* * *
+* * *
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+1  <= N <= 25
+Time Limit: 1 sec
+Sample Input 1:
+3
+Sample Output 1:
+* * *
+* * *
+* * *
+Explanation Of Sample Input 1 :
+For N = 3, fill all the rows and columns in 3x3 matrix with ‘*’.
+Sample Input 2 :
+1
+Sample Output 2 :
+*
+Sample Input 3 :
+4
+Sample Output 3 :
+* * * *
+* * * *
+* * * *
+* * * *
+
+
+  
+:::::::::::::::Solution:::::::::::::::
+
+import java.util.*;
+public class Solution {
+    public static void nForest(int n) {
+        // Write your code here
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
