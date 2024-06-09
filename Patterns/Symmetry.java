ed999c65cb963354dd1e78a9fb3669557cf92d4a
bhargavi8055 <59086546+bhargavi8055@users.noreply.github.com>
2024-06-09 21:02:31 +0530

    Create  Symmetry.java

---
+++ Patterns/Symmetry.java
@@ -0,0 +1,32 @@
+=========Question Link =========
+  https://www.naukri.com/code360/problems/symmetry_6581914?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems
+========Solution ==========
+  public class Solution {
+    public static void symmetry(int n) {
+        // Write your code here
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*"+" ");
+            }
+            for(int j=1;j<=(n*2)-(n-i+1)*2;j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=(n-i+1);j++){
+                System.out.print("*"+" ");
+            }
+            for(int j=1;j<=(n*2)-(n-i+1)*2;j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=(n-i+1);j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
